328a470eb52ebf1d78d5700053cc251667a7e1e3
Ritik <ritikkhulbe22@gmail.com>
2024-02-24 14:31:40 +0530

    util codes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import dotenv from "dotenv";
-import { DB_NAME } from "./constants.js";
+import express from "express";
 import { connectDB } from "./db/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
 
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.error((err) => {
+    console.log("Mongo DB connection failed")
+});
